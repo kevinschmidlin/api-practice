ace7890524078b33e573c295dad1f37697348715
Kevin Schmidlin <87376656+kevinschmidlin@users.noreply.github.com>
2022-12-09 18:00:43 +0800

    Updates

--- api-practice/src/main/java/org/example/Main.java
+++ api-practice/src/main/java/org/example/Main.java
@@ -1,44 +1,62 @@
 package org.example;
 
-import java.net.URLEncoder;
-
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+import java.net.URLEncoder;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         String host = "https://movie-database-alternative.p.rapidapi.com/"; // Host URL
         String charset = "UTF-8";
 
         // Headers for request
-        String x_rapidapi_host = "movie-database-alternative.p.rapidapi.com";
-        String x_rapidapi_key = "4433791c03msh107ae744fef2135p1b458ajsna82004c66e46";
+        String xRapidAPIHost = "movie-database-alternative.p.rapidapi.com";
+        String xRapidAPIKey = "4433791c03msh107ae744fef2135p1b458ajsna82004c66e46";
 
-        // Required parameters
-        String s = "Pulp";
+        boolean isValid = false;
+        Scanner input = new Scanner(System.in);
 
-        // Format query to prevent encoding problems
-        String query = String.format("s=%s", URLEncoder.encode(s, charset));
+        do {
+            System.out.println("1. By Search");
+            System.out.println("2. Quit");
 
-        HttpResponse<JsonNode> response = Unirest.get(host + "?" + query)
-                .header("x-rapidapi-host", x_rapidapi_host)
-                .header("x-rapidapi-key", x_rapidapi_key)
-                .asJson();
+            int option = Integer.parseInt(input.nextLine());
 
-        //System.out.println(response.getStatus());
-        //System.out.println(response.getHeaders().get("Content-Type"));
+            switch (option) {
+                case 1:
+                    System.out.println("Search: ");
 
-        // Prettify
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        JsonParser jp = new JsonParser();
-        JsonElement je = jp.parse(response.getBody().toString());
-        String prettyJsonString = gson.toJson(je);
-        System.out.println(prettyJsonString);
+                    // Required parameters
+                    String s = input.nextLine();
+
+                    // Format query to prevent encoding problems
+                    String query = String.format("s=%s", URLEncoder.encode(s, charset));
+
+                    HttpResponse<JsonNode> response = Unirest.get(host + "?" + query)
+                            .header("x-rapidapi-host", xRapidAPIHost)
+                            .header("x-rapidapi-key", xRapidAPIKey)
+                            .asJson();
+
+                    // Prettify
+                    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                    JsonParser jp = new JsonParser();
+                    JsonElement je = jp.parse(response.getBody().toString());
+                    String prettyJsonString = gson.toJson(je);
+                    System.out.println(prettyJsonString);
+                    break;
+                case 2:
+                    isValid = true;
+                    break;
+            }
+        } while (!isValid);
+
+        System.out.println("Goodbye.");
     }
 }
